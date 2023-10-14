526179076341e55810dabc44360237d21f57db21
Andrés Alcarraz <alcarraz@gmail.com>
2023-10-14 18:55:41 -0500

    First version

---
+++ src/c++-basics.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <string>
+
+class Shape {
+
+public:
+    virtual std::string description() {
+        return "I'm  a shape";
+    }
+
+    virtual ~Shape() = default;
+
+};
+
+class Circle : public Shape {
+    double radius;
+public:
+    Circle(double radius): radius(radius) {}
+    double get_radius() {return radius;}
+
+    std::string description() override{
+        return "I'm a Circle";
+    }
+};
+
+int main() {
+    using namespace std;
+    Shape shape;
+    cout << "description of shape:  " << shape.description() << endl;
+    //description of shape:  I'm  a shape
+
+    Circle circle{4};
+    cout << "description of circle: " << circle.description() << endl;
+    //description of circle: I'm  a shape
+
+    cout << "radius of circle: " << circle.get_radius() << endl;
+    //radius of circle: 4
+
+    return 0;
+}
+
+
